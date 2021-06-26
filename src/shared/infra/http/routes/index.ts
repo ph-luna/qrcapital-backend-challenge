54cdd073c58e00afa3135a1fd934d7c10579d931
Pedro H <ph.luna.vieira@gmail.com>
2021-06-26 16:24:00 -0300

    eslint fix

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -3,7 +3,7 @@ import { Router } from 'express'
 const serverRouter = Router()
 
 serverRouter.get('/', (req, res) => {
-    res.json({ okay: true })
+  res.json({ okay: true })
 })
 
 export default serverRouter
--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -3,11 +3,10 @@ import express from 'express'
 import serverRoutes from './routes'
 
 const server = express()
-const PORT = 3333
+const PORT = 7777
 
-server.use(serverRoutes);
-
+server.use(serverRoutes)
 
 server.listen(PORT, () => {
-    console.log(`O servidor esta online! PORTA: ${PORT}`)
+  console.log(`O servidor esta online! PORTA: ${PORT}`)
 })
