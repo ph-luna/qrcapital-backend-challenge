0d535beb854021fe4863a4b4efa5967cb983ad87
Pedro H <ph.luna.vieira@gmail.com>
2021-06-27 01:58:46 -0300

    db connection in production

--- babel.config.js
+++ babel.config.js
@@ -12,8 +12,9 @@ module.exports = {
     ],
 
     plugins: [
+        "babel-plugin-transform-typescript-metadata",
         ["@babel/plugin-proposal-decorators", { "legacy": true }],
-        "@babel/plugin-proposal-class-properties",
+        ["@babel/plugin-proposal-class-properties", { "loose": true }],
         ['module-resolver', {
             alias: {
                 '@modules': './src/modules',
--- src/shared/typeorm/index.ts
+++ src/shared/typeorm/index.ts
@@ -1,3 +1,21 @@
 import { createConnection } from 'typeorm'
 
-createConnection()
+if (process.env.NODE_ENV === 'production') {
+  createConnection({
+    type: 'postgres',
+    url: 'postgres://uzyqhktp:BS8NoM814eGLeeQwdQr6gbtlczFTdSIT@tuffi.db.elephantsql.com/uzyqhktp',
+    synchronize: true,
+    logging: true,
+    migrations: [
+      './dist/shared/typeorm/migrations/*.js'
+    ],
+    entities: [
+      './dist/modules/**/infra/typeorm/entities/*.js'
+    ],
+    cli: {
+      migrationsDir: './src/shared/typeorm/migrations'
+    }
+  })
+} else {
+  createConnection()
+}
